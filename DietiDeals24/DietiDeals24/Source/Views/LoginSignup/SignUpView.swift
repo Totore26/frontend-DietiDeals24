0186412627e10d08f1ab909c3f9858419debd37d
Totore <tortora2611@gmail.com>
2024-02-23 18:37:58 +0100

    GRANDE MIGLIORAMENTO SHEET PER CONFERMARE L EMAIL

--- DietiDeals24/DietiDeals24/Source/Views/LoginSignup/SignUpView.swift
+++ DietiDeals24/DietiDeals24/Source/Views/LoginSignup/SignUpView.swift
@@ -7,6 +7,7 @@ struct SignUpView: View {
     @State private var password = ""
     @State private var repeatPassword = ""
     @State private var telephoneNumber = ""
+    @State private var otp = ""
     @State private var selectedUserType: FormattedUserType = .buyer
     @EnvironmentObject var sessionManager: SessionManager
     
@@ -65,8 +66,31 @@ struct SignUpView: View {
                     SocialLoginButtonsView()
                 }
                 .padding(.horizontal, 20)
-                .alert(isPresented: $isConfirmationAlertPresented) {
-                    Alert(title: Text("Confirmation Sent"), message: Text(confirmationMessage), dismissButton: .default(Text("OK")))
+                .adaptiveSheet(isPresented: $isConfirmationAlertPresented, detents: [.medium()], smallestUndimmedDetentIdentifier: .large) {
+                    //contenuto della sheet con campo per inserimento del codice di verifica e bottone per confermare
+                    VStack {
+                        
+                        Spacer()
+                        
+                        Text("Insert OTP code sended by mail")
+                        
+                        FormattedTextField(title: "Enter OTP", text: $otp)
+                        
+                        Spacer()
+                        
+                        Button(action: continueSignUp ) {
+                            Text("Continue")
+                                .font(.custom("SF Pro", size: 22))
+                                .fontWeight(.bold)
+                                .padding(.horizontal, 20)
+                                .padding(.vertical, 14)
+                                .frame(width: 300, height: 44)
+                                .background(Color(red: 0.06, green: 0.45, blue: 0.64))
+                                .cornerRadius(14)
+                                .foregroundColor(.white)
+                        }
+                        .padding(.bottom, 70)
+                    }
                 }
             }
         }
@@ -91,7 +115,32 @@ struct SignUpView: View {
             }
         }
     }
+    
+    private func continueSignUp() {
+        async {
+            do {
+                try await sessionManager.confirmSignUp(for: email, with: otp)
+                // La conferma è avvenuta con successo
+                print("Sign up confirmed successfully")
+                
+                // Puoi aggiungere ulteriori azioni dopo la conferma, ad esempio, navigare a un'altra vista
+                // o mostrare un messaggio di conferma all'utente
+            } catch {
+                print("Error during sign up confirmation:", error)
+                // Gestisci l'errore in base alle tue esigenze
+            }
+            
+            // Chiudi la sheet dopo la conferma
+            isConfirmationAlertPresented = false
+            
+            sessionManager.showLogin()
+        }
+    }
+    
+    
 }
+
+
 
 struct SignUpView_Previews: PreviewProvider {
     static var previews: some View {
