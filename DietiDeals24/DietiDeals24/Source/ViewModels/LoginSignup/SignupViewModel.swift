6f4867b609017e324e9a9811046850d6b4df3cb4
Francesco <119133894+terrecruis@users.noreply.github.com>
2024-02-25 11:32:41 +0100

    alcuni appunti sul viewmodel del signup ; Alcuni bug riscontrati e segnati su Notion

--- DietiDeals24/DietiDeals24/Source/ViewModels/LoginSignup/SignupViewModel.swift
+++ DietiDeals24/DietiDeals24/Source/ViewModels/LoginSignup/SignupViewModel.swift
@@ -11,4 +11,9 @@ import Foundation
 class SignUpViewModel {
     
     
+    /*
+        MARK: aggiungi la logica per fare  la richiesta http per aggiungere l'utente creato al database.
+        e inoltre crea un collegamento con le variabili che si trovano all'interno della view corrispondente.
+    */
+    
 }
